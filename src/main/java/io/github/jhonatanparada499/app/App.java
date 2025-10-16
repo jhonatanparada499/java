6e5403b45ce88f8c6130b5f5eed6fbd693d671b1
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-10-16 19:42:59 -0400

    refactor: add temporal ignore lines to allow compilation

--- src/main/java/io/github/jhonatanparada499/app/App.java
+++ src/main/java/io/github/jhonatanparada499/app/App.java
@@ -44,27 +44,27 @@ public class App {
      * CHAPTER 5: Classes II
      * ========================================
      */
-    ToyClass var1 = new ToyClass("Joe", 42);
-    ToyClass var2 = var1;
-    var2.set("Josephine", 2);
-    System.out.println(var1);
+    // ToyClass var1 = new ToyClass("Joe", 42);
+    // ToyClass var2 = var1;
+    // var2.set("Josephine", 2);
+    // System.out.println(var1);
 
     /*
      * ========================================
      * CHAPTER 4: Classes I
      * ========================================
      */
-    DateFirstTry date2 = new DateFirstTry(
-        1, 1, 1),
-        date3 = new DateFirstTry();
+    // DateFirstTry date2 = new DateFirstTry(
+    // 1, 1, 1),
+    // date3 = new DateFirstTry();
 
     Scanner keyboard = new Scanner(System.in);
     System.out.println("Enter a line message");
     String s = keyboard.nextLine();
     System.out.println(s);
 
-    date2.setDate(1, 1, 1);
-    date3.setDate(2, 1, 1);
+    // date2.setDate(1, 1, 1);
+    // date3.setDate(2, 1, 1);
 
     StringTokenizer wordFactory = new StringTokenizer(
         "Give a message, friend.", " ");
