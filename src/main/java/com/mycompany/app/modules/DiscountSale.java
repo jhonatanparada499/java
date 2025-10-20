ea37637e3ae23958891226252e9743a74fe5a68e
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-10-20 19:34:30 -0400

    feat: add Sale class

---
+++ src/main/java/com/mycompany/app/modules/DiscountSale.java
@@ -0,0 +1,67 @@
+package com.mycompany.app.modules;
+
+/**
+ * Class for a sale of one item with discount expressed as a percent of
+ * the price, but no other adjustments.
+ * Class invariant: The price is always nonnegative; the name is a
+ * nonempty string; the discount is always nonnegative.
+ */
+public class DiscountSale extends Sale {
+  private double discount; // A percent of the price. Cannot be negative.
+
+  public DiscountSale() {
+    super();
+    discount = 0;
+  }
+
+  /**
+   * Precondition: theName is a nonempty string; thePrice is
+   * nonnegative; theDiscount is expressed as a percent of the price
+   * and is nonnegative.
+   */
+  public DiscountSale(String theName, double thePrice, double theDiscount) {
+    super(theName, thePrice);
+    setDiscount(theDiscount);
+  }
+
+  public DiscountSale(DiscountSale originalObject) {
+    super(originalObject);
+    discount = originalObject.discount;
+  }
+
+  public static void announcement() {
+    System.out.println("This is the DiscountSale class.");
+  }
+
+  public double getDiscount() {
+    return discount;
+  }
+
+  public double bill() {
+    double fraction = discount / 100;
+    return (1 - fraction) * getPrice();
+  }
+
+  /**
+   * Precondition: Discount is nonnegative.
+   */
+  public void setDiscount(double newDiscount) {
+    if (newDiscount >= 0)
+      discount = newDiscount;
+    else {
+      System.out.println("Error: Negative discount.");
+      System.exit(0);
+    }
+  }
+
+  public String toString() {
+    return (getName() + " Price = $" + getPrice()
+        + " Discount = " + discount + "%\n"
+        + " Total cost = $" + bill());
+  }
+
+  public boolean equals(Object otherObject) {
+    // The rest of the definition of equals is located in Self-Test Exercise 4.
+    return false; // placeholder
+  }
+}
