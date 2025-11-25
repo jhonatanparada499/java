540289124748b1ea6cafad4120807b618b12d049
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-25 18:25:33 -0500

    feat: add work with GUIs using swing

---
+++ src/main/java/com/mycompany/app/GuiApp.java
@@ -0,0 +1,31 @@
+package com.mycompany.app;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import com.mycompany.app.listeners.*;
+
+public class GuiApp {
+  public static final int WIDTH = 400;
+  public static final int HEIGHT = 300;
+
+  public static void main(String[] args) {
+    /*
+     * ========================================
+     * CHAPTER 17: Buttons, Events and Other ...
+     * ========================================
+     */
+    JFrame firstWindow = new JFrame();
+    firstWindow.setSize(WIDTH, HEIGHT);
+    firstWindow.setVisible(true);
+    firstWindow.setDefaultCloseOperation(
+        JFrame.DO_NOTHING_ON_CLOSE);
+
+    JButton endButton = new JButton(
+        "Click to end program");
+    endButton.addActionListener(
+        new EndingListener());
+
+    firstWindow.add(endButton);
+    firstWindow.setVisible(true);
+  }
+}
---
+++ src/main/java/com/mycompany/app/listeners/EndingListener.java
@@ -0,0 +1,10 @@
+package com.mycompany.app.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EndingListener implements ActionListener {
+  public void actionPerformed(ActionEvent e) {
+    System.exit(0);
+  }
+}
