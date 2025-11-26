7b469cce29700cfc54e1eba1d30b6cf36dd0e212
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-26 17:30:01 -0500

    feat: add repository for graphical interfaces

---
+++ src/main/java/com/mycompany/app/windows/MainWindow.java
@@ -0,0 +1,28 @@
+package com.mycompany.app.windows;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import com.mycompany.app.listeners.EndingListener;
+
+public class MainWindow extends JFrame {
+  public static final int WIDTH = 400;
+  public static final int HEIGHT = 300;
+
+  public MainWindow() {
+    super();
+
+    setSize(WIDTH, HEIGHT);
+    setTitle("A title");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JLabel label = new JLabel("Hello World");
+
+    // JButton endButton = new JButton("Click to end program");
+    // endButton.addActionListener(new EndingListener());
+    //
+    // add(endButton);
+    add(label);
+  }
+}
